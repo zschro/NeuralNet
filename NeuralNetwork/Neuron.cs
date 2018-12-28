ba359d5ff510c51fb3d390fe7c868425ab33388d
Zachary Schroeder <z.schro@gmail.com>
2018-12-28 13:27:02 -0500

    first commit, basic neuron

---
+++ NeuralNetwork/Neuron.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        private double inputWeight1;
+        private double inputWeight2;
+        private double bias;
+
+        public Neuron()
+        {
+            InputWeight1 = GetSmallRandom();
+            InputWeight2 = GetSmallRandom();
+            Bias = GetSmallRandom();
+        }
+
+        public double InputWeight1 { get => inputWeight1; set => inputWeight1 = value; }
+        public double InputWeight2 { get => inputWeight2; set => inputWeight2 = value; }
+        public double Bias { get => bias; set => bias = value; }
+
+        private double GetSmallRandom()
+        {
+            var random = new Random();
+            return random.NextDouble() * 2 - 1;
+        }
+
+    }
+    public class FiringNeuron
+    {
+        private Neuron _neuron;
+        public FiringNeuron(Neuron neuron)
+        {
+            _neuron = neuron;
+        }
+        public double Fire(double input1, double input2)
+        {
+            var totalInput = input1 * _neuron.InputWeight1 +
+                input2 * _neuron.InputWeight2 +
+                _neuron.Bias;
+
+            var output = totalInput > 0 ? totalInput : 0; //ReLu
+            return output;
+        }
+        //learn -> 
+
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var neuron = new Neuron();
+            var firingNeuron = new FiringNeuron(neuron);
+            var result = firingNeuron.Fire(.1,.1);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static List<DataPoint> GenerateTestData(int size)
+        {
+            var r = new Random();
+            bool input1 = r.Next(0,2) >= 1;
+            bool input2 = r.Next(0,2) >= 1;
+            var result = new DataPoint()
+            {
+                Input1 = (double)input1,
+                Input2 = (double)input2,
+                Output = (double) !(input1 | input2)
+            }
+        }
+    }
+    public class DataPoint
+    {
+        public double Input1;
+        public double Input2;
+        public double Output;
+    }
+}
