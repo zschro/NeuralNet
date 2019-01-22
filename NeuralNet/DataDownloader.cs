d37b916d2c9eefbb9122417ca2aa810408abe858
Zachary Schroeder <z.schro@gmail.com>
2019-01-22 08:26:14 -0500

    setup data downloader

---
+++ NeuralNet/DataDownloader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace NeuralNet
+{
+    public static class DataDownloader
+    {
+        public static void DownloadMNIST()
+        {
+            using (var client = new WebClient())
+            {
+                if(!File.Exists($"{Directory.GetCurrentDirectory()}/train-images-idx3-ubyte.gz"))
+                    client.DownloadFile("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "train-images-idx3-ubyte.gz");
+
+                client.DownloadFile("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz", "train-labels-idx1-ubyte.gz");
+                client.DownloadFile("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz", "t10k-images-idx3-ubyte.gz");
+                client.DownloadFile("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz", "t10k-labels-idx1-ubyte.gz");
+                foreach (FileInfo fileToDecompress in new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles("*.gz"))
+                {
+                    Decompress(fileToDecompress);
+                }
+            }
+        }
+        public static void Decompress(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Console.WriteLine("Decompressed: {0}", fileToDecompress.Name);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NeuralNet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeuralNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataDownloader.DownloadMNIST();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
